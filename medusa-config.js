056bc57aa06735e7dfd85f6c523efe3c7e9df446
shadowcz007 <chizhiwei007@163.com>
2023-12-05 16:40:01 +0800

    1

--- medusa-config.js
+++ medusa-config.js
@@ -41,6 +41,8 @@ const plugins = [
     options: {
       api_key: process.env.STRIPE_API_KEY,
       webhook_secret: process.env.STRIPE_WEBHOOK_SECRET,
+      capture:true,
+      automatic_payment_methods:true
     },
   },
   {
